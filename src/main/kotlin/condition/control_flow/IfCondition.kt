91a9fd3768eb088f1b14a75743dd9ba59810fa69
Wacharat <wacharat@odds.team>
2019-05-28 21:10:07 +0700

    create example for use if condition

---
+++ src/main/kotlin/condition/control_flow/IfCondition.kt
@@ -0,0 +1,21 @@
+package condition.control_flow
+
+class IfCondition {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            var a = 1
+            val max = 10
+
+            //TODO:: 1.tranditional usage
+            if (a < max) a += 1
+
+            //TODO:: 2.with else
+            if (a < max) {
+                a += 1
+            } else {
+                println("Maximum")
+            }
+        }
+    }
+}

43556054c9e3fe9ffaea2b1a8f0d73fe247ef875
Wacharat <wacharat@odds.team>
2019-06-02 14:27:27 +0700

    eample access modifier in a class

---
+++ src/main/kotlin/modifier/FinalAndOpenExample.kt
@@ -0,0 +1,33 @@
+package modifier
+
+class FinalAndOpenExample{
+
+    /*TODO::
+    * 1.(Final) cannot override class and can not visible class member
+    * 2.(Open) can override and specified explicitly
+    */
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val human = Human("Jack")
+            /*TODO:: 1.2 final by default can't visible name
+            * human.name
+            */
+
+            //TODO:: visible class member when specified explicitly
+            val human1 = Human1("Jackle")
+            println(human1.name)
+        }
+    }
+}
+
+class Human(val name: String)
+
+/* TODO:: 1.1 class Person cannot override class Human
+* class Person(): Human
+*/
+
+
+open class Human1(val name: String)
+// TODO:: 2.1 Override open class Human1 and specified explicitly of class member are mutable(var) or immutable(val)
+class Person(name: String): Human1(name)

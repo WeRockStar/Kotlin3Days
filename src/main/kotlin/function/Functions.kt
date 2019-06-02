391832611fb98210c4cc663b282f007860b906f7
WeRockStar <kotchaphan.m@gmail.com>
2019-06-02 15:11:40 +0700

    Add example extenstion function

--- src/main/kotlin/function/Functions.kt
+++ src/main/kotlin/function/Functions.kt
@@ -1,2 +1,11 @@
 package function
 
+
+fun main() {
+    //TODO: Extension function
+    "1".toInt()
+    val name = listOf("WeRockStar", "Yao")
+    name.map { it.toMr() }
+}
+
+fun String.toMr() = "Mr.$this"

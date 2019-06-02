44c5faea42755382f7ab73d33587bb8bd412410d
Wacharat <wacharat@odds.team>
2019-06-02 13:03:52 +0700

    for loops example

--- src/main/kotlin/condition/loops/LoopsExample.kt
+++ src/main/kotlin/condition/loops/LoopsExample.kt
@@ -1,6 +1,35 @@
 package condition.loops
 
 class LoopsExample {
-    //TODO:: 1.For loop
+    /*TODO::
+        1.For loop
+        2.difind with out block
+        3.for loop use index
+        4.for looop value with index
+    */
 
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            //TODO:: 1.For loop
+            val arrayNumber = arrayOf(1, 2, 3)
+            for (number in arrayNumber) {
+                println(number)
+            }
+
+            //TODO:: 2.difind with out block
+            for (number in arrayNumber)
+                println(number)
+
+            //TODO:: 3.for loop use index
+            for (index in arrayNumber.distinct()) {
+                println(arrayNumber[index])
+            }
+
+            //TODO:: 4.for looop value with index
+            for ((index, value) in arrayNumber.withIndex()) {
+                println("Element at $index is $value")
+            }
+        }
+    }
 }

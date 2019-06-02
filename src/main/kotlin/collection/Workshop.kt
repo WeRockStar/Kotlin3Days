5fdc244b7c9d26aafa2a88765b35f9dfb1082934
WeRockStar <kotchaphan.m@gmail.com>
2019-06-02 16:19:31 +0700

    Add for-loop code for refactor workshop

--- src/main/kotlin/collection/Workshop.kt
+++ src/main/kotlin/collection/Workshop.kt
@@ -2,4 +2,19 @@ package collection
 
 fun main() {
 
+    val numbers = listOf("1", "2", "3", "4", "5", "6", "7", "8")
+    val evenNumber = mutableListOf<Int>()
+    for (number in numbers) {
+        val n = number.toInt()
+        if (n % 2 == 0) {
+            evenNumber.add(n)
+        }
+    }
+    var sum = 0
+    for (number in evenNumber) {
+        sum += number
+    }
+
+    println(sum)
+
 }
